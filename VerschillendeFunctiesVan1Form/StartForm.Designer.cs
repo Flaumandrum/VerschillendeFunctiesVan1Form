3f6432040e3b64510c7acd1bc6486eb107fc03f3
Tom Adriaens <tomadriaens@hotmail.com>
2019-04-12 11:11:14 +0200

    start

---
+++ VerschillendeFunctiesVan1Form/StartForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace VerschillendeFunctiesVan1Form
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToevoegen = new System.Windows.Forms.Button();
+            this.btnAanpassen = new System.Windows.Forms.Button();
+            this.btnVerwijderen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnToevoegen
+            // 
+            this.btnToevoegen.Location = new System.Drawing.Point(262, 83);
+            this.btnToevoegen.Name = "btnToevoegen";
+            this.btnToevoegen.Size = new System.Drawing.Size(75, 23);
+            this.btnToevoegen.TabIndex = 0;
+            this.btnToevoegen.Text = "Toevoegen";
+            this.btnToevoegen.UseVisualStyleBackColor = true;
+            // 
+            // btnAanpassen
+            // 
+            this.btnAanpassen.Location = new System.Drawing.Point(262, 132);
+            this.btnAanpassen.Name = "btnAanpassen";
+            this.btnAanpassen.Size = new System.Drawing.Size(75, 23);
+            this.btnAanpassen.TabIndex = 1;
+            this.btnAanpassen.Text = "Aanpassen";
+            this.btnAanpassen.UseVisualStyleBackColor = true;
+            // 
+            // btnVerwijderen
+            // 
+            this.btnVerwijderen.Location = new System.Drawing.Point(262, 192);
+            this.btnVerwijderen.Name = "btnVerwijderen";
+            this.btnVerwijderen.Size = new System.Drawing.Size(75, 23);
+            this.btnVerwijderen.TabIndex = 2;
+            this.btnVerwijderen.Text = "Verwijderen";
+            this.btnVerwijderen.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerwijderen);
+            this.Controls.Add(this.btnAanpassen);
+            this.Controls.Add(this.btnToevoegen);
+            this.Name = "StartForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToevoegen;
+        private System.Windows.Forms.Button btnAanpassen;
+        private System.Windows.Forms.Button btnVerwijderen;
+    }
+}
+
